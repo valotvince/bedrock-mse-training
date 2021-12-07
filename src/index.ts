0636fa22888d45588d17836617d77de259ef73ae
Vincent Valot <vincent.valot@bedrockstreaming.com>
2021-12-07 13:37:59 +0100

    step 2 - add audio segments in sync with video segments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getSegments, getMimeType, findSegment, findNearestCompleteSegment } from './utils/segments';
 import { fetchSegment } from './utils/networking';
 import { getBufferedRange } from './utils/buffer';
+import { Segment } from './types';
 
 // This is a list of clear video segments
 const videoSegments = getSegments('video');
@@ -16,32 +17,62 @@ const mediaSource = new MediaSource();
 
 const getPlaybackPosition = () => videoTag.currentTime;
 
-async function onMediaSourceOpen() {
-  const buffer = mediaSource.addSourceBuffer(videoMimeType);
+class BufferManager {
+  buffer: SourceBuffer;
+  queue: Segment[] = [];
+  mimeType: string;
 
-  const segmentsData: ArrayBuffer[] = [];
+  constructor(mediaSource: MediaSource, mimeType: string) {
+    this.buffer = mediaSource.addSourceBuffer(mimeType);
+    this.mimeType = mimeType;
 
-  // Listen for when the buffer ends updating, and add the queued segments data
-  buffer.addEventListener('updateend', () => {
-    const data = segmentsData.shift();
+    // Listen for when the buffer ends updating, and add the queued segments data
+    this.buffer.addEventListener('updateend', this.onBufferUpdateEnd);
+  }
 
-    if (data) {
-      buffer.appendBuffer(data);
+  onBufferUpdateEnd = () => {
+    if (this.buffer.updating) {
+      return;
     }
-  });
 
-  // Loop through the known videoSegments in order
-  for (const segment of videoSegments) {
-    const segmentData = await fetchSegment(segment.url);
+    const segment = this.queue.shift();
+
+    if (segment && segment.data) {
+      this.buffer.appendBuffer(segment.data);
+    }
+  };
+
+  async fetchAndQueueSegment(segment: Segment) {
+    // Set data only if it hasn't been fetched already
+    if (!segment.data) {
+      segment.data = await fetchSegment(segment.url);
+    }
 
     // If the buffer is currently appending buffer, we should queue the segment
     // and wait for the operation to finish before adding any more buffer.
-    if (buffer.updating) {
-      segmentsData.push(segmentData);
+    if (this.buffer.updating) {
+      this.queue.push(segment);
     } else {
-      buffer.appendBuffer(segmentData);
+      this.buffer.appendBuffer(segment.data);
     }
   }
+
+  async schedule(segments: Segment[]) {
+    // Loop through the known videoSegments in order
+    for (const segment of segments) {
+      await this.fetchAndQueueSegment(segment);
+    }
+  }
+}
+
+async function onMediaSourceOpen() {
+  // Create two buffers holding video and audio. MediaSource will handle the sync between them
+  const videoBuffer = new BufferManager(mediaSource, videoMimeType);
+  const audioBuffer = new BufferManager(mediaSource, audioMimeType);
+
+  // Feed segments to both video and audio
+  videoBuffer.schedule(videoSegments);
+  audioBuffer.schedule(audioSegments);
 }
 
 window.addEventListener('load', () => {
